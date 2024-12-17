e808986ad61791d1034bc50eaa3f1139eef030a0
unicornglitterpoop <761368@student.dusd.net>
2024-12-17 10:35:55 -0800

    almost finished project
    recorded game

--- FlappyBirdP2Payan/Assets/Scripts 1/GameControl.cs
+++ FlappyBirdP2Payan/Assets/Scripts 1/GameControl.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public Text scoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+
+    private int score = 0;
     // Start is called before the first frame update
     void Awake()
     {
@@ -30,7 +34,15 @@ if (gameOver == true && Input.GetMouseButtonDown(0))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
-
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score:" + score.ToString();
+    }
     public void BirdDied()
     {
         gameOverText.SetActive (true);
